7e1503a36afb3a55a7cdda54d02b27ae94377419
Ali Naqi Khan <90995492+alinaqikhan@users.noreply.github.com>
2022-08-31 03:40:12 +0500

    Added mobile navigation menu

--- components/layouts/navigation-bar.js
+++ components/layouts/navigation-bar.js
@@ -6,8 +6,10 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { MenuIcon } from '../icons';
 import { NavLink } from './nav-link';
+import { useState } from 'react';
 
 const NAVIGATION_LINKS = [
   { id: 1, title: 'Home', href: '/' },
@@ -18,11 +20,22 @@ const NAVIGATION_LINKS = [
 ];
 
 const NavigationBar = props => {
+
+  const [toggle, setToggle] = useState(false);
+
+  const openMenuHandler = () => {
+    setToggle(true);
+  };
+
+  const closeMenuHandler = () => {
+    setToggle(false);
+  };
+
   return (
     <nav className="navbar-bg h-14 px-2 md:px-6 lg:px-12">
-      <div className="h-full flex items-center justify-between lg:hidden">
-        <IconButton aria-label="Menu">
-          <MenuIcon />
+      <div className="h-full flex items-center justify-between lg:hidden relative">
+        <IconButton aria-label="Menu" onClick={()=>setToggle(toggle=>!toggle)}>
+          {toggle === false? <MenuIcon />:<CloseOutlinedIcon style={{height:30,width:30,color:"white"}} />}
         </IconButton>
         <Link href={'/'}>
           <a className="text-white font-medium text-lg">UET, Tribune</a>
@@ -39,6 +52,15 @@ const NavigationBar = props => {
           <WhatsAppIcon style={{ color: 'white', height: 30, width: 30 }} />
         </IconButton>
       </div>
+
+        {toggle && <div className='overlay' onClick={closeMenuHandler}>
+            <div className='w-4/6 h-full mobile-nav-menu'>
+            <ul className='flex flex-col px-5 py-7 text-black gap-5 h-full'>
+            {NAVIGATION_LINKS.map(link => <NavLink key={link.id} title={link.title} href={link.href} />)}
+        </ul>
+            </div>
+          </div>}
+
       <div className="h-full hidden lg:flex items-center justify-between">
         <ul className='flex text-white gap-5 h-full'>
             {NAVIGATION_LINKS.map(link => <NavLink key={link.id} title={link.title} href={link.href} />)}
