1fd37a0d4ab7d23e33adc303fe20d8f6e9267a2a
Ali Naqi Khan <90995492+alinaqikhan@users.noreply.github.com>
2022-08-31 19:43:13 +0500

    Fixed navigation bar to stay on top

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -3,10 +3,10 @@ import NavigationBar from './navigation-bar';
 const Layout = props => {
     return (
         <>
-        <header>
+        <header className='fixed w-full z-50 top-0 left-0'>
             <NavigationBar />
         </header>
-        <main className='mx-4 my-3 md:mx-8 md:my-5 lg:mx-12'>
+        <main className='py-14 mx-4 my-3 md:mx-8 md:my-5 lg:mx-12'>
             {props.children}
         </main>
         <footer>
