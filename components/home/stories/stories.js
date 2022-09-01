d54be4a445db289ab520de0e408ab1a23b848654
Ali Naqi Khan <90995492+alinaqikhan@users.noreply.github.com>
2022-09-01 22:31:10 +0500

    Added stories section in home page

--- components/home/stories/stories.js
+++ components/home/stories/stories.js
@@ -1,9 +1,37 @@
+import { Divider } from '@mui/material';
+import Image from 'next/image';
+
 const Stories = props => {
-    return (
-        <section className="h-96 w-full bg-green-400">
-            
-        </section>
-    );
+  return (
+    <section className="h-96 w-full">
+      <h2 className="stories-top-border m-2 font-bold text-gray-900 text-xl pt-1 md:text-center">
+        Stories
+      </h2>
+      <div>
+        <StoryItem />
+        <StoryItem />
+        <StoryItem />
+        <StoryItem />
+        <StoryItem />
+        <StoryItem />
+      </div>
+    </section>
+  );
 };
 
 export default Stories;
+
+const StoryItem = props => {
+  return (
+    <>
+    <div className="m-2">
+      <div className='flex gap-2'>
+        <Image src={'/story.jpg'} width={60} height={60} />
+        <p className='truncate-5 text-xs w-3/4 hover:underline'>Artwork by Ghulfam Ali Khan, from Mechatronics and Control Engineering Department, Session 20, UET Lahore (Faisalabad Campus).</p>
+      </div>
+      <p className="tag mt-2 pl-1 text-gray-900">Campus Art</p>
+    </div>
+    <Divider className="m-3" />
+    </>
+  );
+};
