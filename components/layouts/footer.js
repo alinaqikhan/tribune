1cc4b9ccff7630e0052a41143fad94d93573e483
Ali Naqi Khan <90995492+alinaqikhan@users.noreply.github.com>
2022-09-02 13:38:50 +0500

    Completed the footer

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -10,73 +10,76 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const Footer = props => {
   return (
     <>
-      <div className='flex max-w-full flex-col md:flex-row'>
-      <div className="p-4 bg-footer-like flex-1 md:px-8 lg:px-12">
-          <h4 className="font-semibold py-1 text-gray-600 border-bottom mb-2">UET Tribune</h4>
-          <p className='text-xs mb-1'>
-          <strong>UET Tribune</strong> is the first ever magazine style news network of UET which brings the <strong>news</strong>, <strong>information</strong>, <strong>alertness</strong>, <strong>awareness</strong> and <strong>social aspects of campus</strong> with its unique style.
+      <div className="flex max-w-full flex-col md:flex-row">
+        <div className="p-4 bg-footer-like flex-1 md:px-8 lg:px-12">
+          <h4 className="font-semibold py-1 text-gray-600 border-bottom mb-2">
+            UET Tribune
+          </h4>
+          <p className="text-xs mb-1">
+            <strong>UET Tribune</strong> is the first ever magazine style news
+            network of UET which brings the <strong>news</strong>,{' '}
+            <strong>information</strong>, <strong>alertness</strong>,{' '}
+            <strong>awareness</strong> and{' '}
+            <strong>social aspects of campus</strong> with its unique style.
           </p>
-          <p className='text-xs mb-1'>
-          Founded in <strong>2015</strong> by <strong>Umar Azam</strong> of Geological Engineering (Session 2012), Co-Founded By <strong>Haris Rana</strong> of Chemical Engineering (Session 2012) and <strong>Waqas Saleem</strong> of Environmental Engineering (Session 2012).
+          <p className="text-xs mb-1">
+            Founded in <strong>2015</strong> by <strong>Umar Azam</strong> of
+            Geological Engineering (Session 2012), Co-Founded By{' '}
+            <strong>Haris Rana</strong> of Chemical Engineering (Session 2012)
+            and <strong>Waqas Saleem</strong> of Environmental Engineering
+            (Session 2012).
           </p>
-          <p className='text-xs'>
-          UET Tribune is the <strong>fastest growing</strong> infotainment based <strong>media network</strong> of UET.
+          <p className="text-xs">
+            UET Tribune is the <strong>fastest growing</strong> infotainment
+            based <strong>media network</strong> of UET.
           </p>
         </div>
         <div className="p-4 bg-footer-like flex-1 md:px-8 lg:px-12">
-          <h4 className="font-semibold py-1 text-gray-600 border-bottom mb-2">Contact</h4>
+          <h4 className="font-semibold py-1 text-gray-600 border-bottom mb-2">
+            Contact
+          </h4>
           <p>
-            <Link href={'/'}>
-              <a>
-                <EmailIcon />
-                <span className="pl-2 text-xs">uettribune@uet.edu.pk</span>
-              </a>
-            </Link>
+            <a href="mailto:uettribune@gmail.com">
+              <EmailIcon />
+              <span className="pl-2 text-xs">uettribune@uet.edu.pk</span>
+            </a>
           </p>
           <p>
-            <Link href={'/'}>
-              <a>
-                <LocationOnOutlinedIcon />
-                <span className="pl-2 text-xs">
-                  University of Engineering and Technology, Lahore
-                </span>
-              </a>
-            </Link>
+            <a href="https://goo.gl/maps/rMgaAJSgbWFUbXxa6">
+              <LocationOnOutlinedIcon />
+              <span className="pl-2 text-xs">
+                University of Engineering and Technology, Lahore
+              </span>
+            </a>
           </p>
         </div>
         <div className="p-4 bg-footer-like flex-1 md:px-8 lg:px-12">
-          <h4 className="font-semibold py-1 text-gray-600 border-bottom mb-2">Follow us</h4>
+          <h4 className="font-semibold py-1 text-gray-600 border-bottom mb-2">
+            Follow us
+          </h4>
           <p>
-            <Link href={'/'}>
-              <a>
-                <FacebookOutlinedIcon color="primary" />
-                <span className="pl-2 text-xs">Facebook</span>
-              </a>
-            </Link>
+            <a href='https://www.facebook.com/uettribune/'>
+              <FacebookOutlinedIcon color="primary" />
+              <span className="pl-2 text-xs">Facebook</span>
+            </a>
           </p>
           <p>
-            <Link href={'/'}>
-              <a>
-                <YouTubeIcon color="error" />
-                <span className="pl-2 text-xs">Youtube</span>
-              </a>
-            </Link>
+            <a href='https://www.youtube.com/channel/UCHXb4jFZo1pu-PvIUJ7wXJA'>
+              <YouTubeIcon color="error" />
+              <span className="pl-2 text-xs">Youtube</span>
+            </a>
           </p>
           <p>
-            <Link href={'/'}>
-              <a>
-                <TwitterIcon style={{ color: '#1d9bf0' }} />
-                <span className="pl-2 text-xs">Twitter</span>
-              </a>
-            </Link>
+            <a href='https://twitter.com/uettribune'>
+              <TwitterIcon style={{ color: '#1d9bf0' }} />
+              <span className="pl-2 text-xs">Twitter</span>
+            </a>
           </p>
           <p>
-            <Link href={'/'}>
-              <a>
-                <LinkedInIcon style={{ color: '#0a66c2' }} />
-                <span className="pl-2 text-xs">LinkedIn</span>
-              </a>
-            </Link>
+            <a href='https://pk.linkedin.com/company/uettribune'>
+              <LinkedInIcon style={{ color: '#0a66c2' }} />
+              <span className="pl-2 text-xs">LinkedIn</span>
+            </a>
           </p>
         </div>
       </div>
